8fad21c9f130b3e3b432bf73e6de2e60d012490b
Sherin1998 <75293746+Sherin1998@users.noreply.github.com>
2021-05-06 09:38:37 +0530

    Add files via upload

---
+++ Assig.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#QUES : 8
+spam = int(input("Enter the value :"))                         
+if(spam==1):
+    print("Hello")
+elif(spam==1):
+    print("Howdy")
+else:
+    print("greetings")
+
+
+# # QUES : 12
+
+# In[3]:
+
+
+for i in range(1,11):
+    print(i)
+
+
+# In[4]:
+
+
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+       
+
+
+# 
+
+# In[ ]:
+
+
+
+
